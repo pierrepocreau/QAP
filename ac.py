f9797fe9f22790d368fc8e13a0770d6eddf5a3fc
pierrepocreau <pierre2629@gmail.com>
2020-03-30 17:33:51 +0200

    arithmetic circuit for matrix * matrix multiplication

--- ac.py
+++ ac.py
@@ -1,6 +1,23 @@
 from wire import Wire
 from gates import Mult, Add, MultScalar
 
+def matMat_AC(A, B):
+    '''
+    Arithmetic circuit for Matrix * Matrix multiplication
+    '''
+    n, k = len(A), len(A[0])
+    m = len(B[0])
+    inputs, outputs = [], []
+    B_col = list(zip(*B))
+
+    for j in range(m):
+        input_, output = matVec_AC(A, B_col[j])
+        inputs.append(input_)
+        outputs.append(output)
+
+    #list(zip(*A)) take the transpose of A
+    return list(zip(*inputs)), list(zip(*outputs))
+
 def matVec_AC(A, X):
     '''
     Arithmetic circuit for Matrice * Vector multiplication.
@@ -37,7 +54,12 @@ def vecVec_AC(A, inputs):
 if __name__ == "__main__":
     A = [[2, 4, 1], [5, 2, 2]]
     X = [1, 2, 3]
+    B = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
     inputs, outputs = matVec_AC(A, X)
+    inputs2, outputs2 = matMat_AC(A, B)
+
     resultats = [output.value for output in outputs]
+    resultats2 = [[output.value for output in ligne] for ligne in outputs2]
     print(resultats)
+    print(resultats2)
